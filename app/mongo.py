05374f8733f03fc387ddd693cf04770c0e177bfc
xtenaz <marcos@xaxas.net>
2021-08-18 21:41:44 -0300

    update and add messages fixed

--- app/mongo.py
+++ app/mongo.py
@@ -2,10 +2,11 @@ from boto3 import client
 from pymongo import MongoClient
 import os
 
-db_address = os.environ['DB_URL']
-db_port = os.environ['DB_PORT']
+# db_address = os.environ['DB_URL']
+# db_port = os.environ['DB_PORT']
 
-client = MongoClient('mongodb://{}:{}'.format(db_address,db_port))
+# client = MongoClient('mongodb://{}:{}'.format(db_address,db_port))
+client = MongoClient('mongodb://34.95.207.192:27017')
 
 print(client.server_info)
 
