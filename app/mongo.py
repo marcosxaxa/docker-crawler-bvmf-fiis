609e7be7f77dfdffad3d79001b078746176c4468
xtenaz <marcos@xaxas.net>
2021-11-26 10:07:42 -0300

    added env variables to connect to mongo atlas

--- app/mongo.py
+++ app/mongo.py
@@ -2,15 +2,16 @@ from boto3 import client
 from pymongo import MongoClient
 import os
 
-db_address = os.environ['DB_URL']
-db_port = os.environ['DB_PORT']
+# db_address = os.environ['DB_URL']
+# db_port = os.environ['DB_PORT']
 
-# username = os.environ['DB_USERNAME']
-# password = os.environ['DB_PASSWORD']
-# name = os.environ['DB_NAME']
+username = os.environ['DB_USERNAME']
+password = os.environ['DB_PASSWORD']
+db_name = os.environ['DB_NAME']
 
-client = MongoClient('mongodb://{}:{}'.format(db_address,db_port))
-# client = MongoClient('mongodb+srv://{}:{}@fii-api.gnuy4.mongodb.net/{}?retryWrites=true&w=majority'.format(username,password,name))
+# client = MongoClient('mongodb://{}:{}'.format(db_address,db_port))
+print(db_name)
+client = MongoClient('mongodb+srv://{}:{}@fii-api.gnuy4.mongodb.net/{}?retryWrites=true&w=majority'.format(username,password,db_name))
 
 
 print(client.server_info)
