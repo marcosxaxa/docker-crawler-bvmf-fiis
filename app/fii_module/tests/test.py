bdfd0d5d062e8fb1dc081c4b92e682a226ef4e75
Marcos Xaxá <marcos.xaxa@serpro.gov.br>
2021-12-09 15:30:46 -0300

    new tests

--- app/fii_module/tests/test.py
+++ app/fii_module/tests/test.py
@@ -9,6 +9,10 @@ class TestFiis(TestCase,Crawler):
     @mock.patch.dict(os.environ, {"DB_NAME": "dev", "DB_PASSWORD": "dev-user", "DB_USERNAME": "dev-user" })
 
     def test_list_fiis(self):
+        username = ""
+        password = ""
+        db_name = ""
+
         fiis_list = Crawler.get_fii_list(self)
         self.assertIn("BCFF11", fiis_list)
 
